626075ceba137ee0531febc3afb9367a27087f1f
ANIK SAHA <46953050+aniksaha12@users.noreply.github.com>
2024-02-07 00:13:24 +0530

    fetching data from api

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
 // Create an element
-const container = document.createElement('div');
-container.id = 'container';
+const container = document.createElement("div");
+container.id = "container";
 
 //Set some attributes
-container.setAttribute('class', 'main-container');
+container.setAttribute("class", "main-container");
 
 //Create a child elements
-const heading = document.createElement('h1');
-heading.textContent = 'Hello,  World!';
+const heading = document.createElement("h1");
+heading.textContent = "Hello,  World!";
 
-const paragraph = document.createElement('p');
-paragraph.textContent = 'This is a simple dom example created with vanilla javascript';
+const paragraph = document.createElement("p");
+paragraph.textContent =
+  "This is a simple dom example created with vanilla javascript";
 
-const button = document.createElement('button');
-button.textContent = 'click me';
+const button = document.createElement("button");
+button.textContent = "click me";
 
 // Create an input field
-const inputField = document.createElement('input');
-inputField.setAttribute('type','text');
-inputField.setAttribute('placeholder', 'Type something');
+const inputField = document.createElement("input");
+inputField.setAttribute("type", "text");
+inputField.setAttribute("placeholder", "Type something");
 
 // define an input event handler
-inputField.addEventListener('input', function(){
-    paragraph.textContent = 'You typed: ' + inputField.value;
-})
-
-// Define a click event handler
-button.addEventListener('click', function(){
-    alert('Thanks for click the button!')
+inputField.addEventListener("input", function () {
+  paragraph.textContent = "You typed: " + inputField.value;
 });
 
+// Define a click event handler
+button.addEventListener("click", function () {
+  alert("Thanks for click the button!");
+});
+
+// fetching data from an API
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((data) => {
+    const apiParagraph = document.createElement("p");
+    const apiUser = document.createElement("h4");
+    apiUser.textContent = "User data: " + data.userId;
+    apiParagraph.textContent = "Fetched data: " + data.title;
+
+    container.appendChild(apiUser);
+    container.appendChild(apiParagraph);
+  })
+  .catch((error) => console.error("Enter fetching data:", error));
 
 // Append child elements to the container
 container.appendChild(heading);
